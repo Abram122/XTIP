7d919eba43ab5088c504f5d568fdbc1d2588c3a6
Abram Mina <114501769+Abram122@users.noreply.github.com>
2025-09-28 00:16:28 +0300

    Update userController.js

--- server/api/Controllers/userController.js
+++ server/api/Controllers/userController.js
@@ -86,10 +86,11 @@ export const loginUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            secure: true, 
+            sameSite: "none", 
             maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+        })
+
 
         res.json({
             message: "Login successful",
