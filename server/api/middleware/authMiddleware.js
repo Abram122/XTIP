1c1a91c140f9fc65de660a4228d37d62359075bf
Abram Mina <114501769+Abram122@users.noreply.github.com>
2025-09-30 00:21:51 +0300

    solve sup 3

--- server/api/middleware/authMiddleware.js
+++ server/api/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import User from "../Models/User.js";
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
