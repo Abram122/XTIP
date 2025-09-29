ed04dfcdc95be0f0a62fff23724fe89e0699903d
MoatazSalah306 <139802667+MoatazSalah306@users.noreply.github.com>
2025-09-29 22:23:02 +0300

    fix: - resolve merge conflicts   "userRoute.js"

--- server/api/Routes/userRoutes.js
+++ server/api/Routes/userRoutes.js
@@ -3,6 +3,9 @@ import {
   registerUser,
   loginUser,
   saveAuthUser,
+  logoutUser,
+  getProfile,
+  updateProfile,
 } from "../Controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -11,8 +14,12 @@ const router = express.Router();
 router.get("/me", protect, (req, res) => {
     res.json({ message: "User data", userId: req.user.id });
 });
+
+router.get("/myprofile", protect, getProfile);
+router.patch("/updateprofile", protect, updateProfile);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/save", saveAuthUser);
+router.post("/logout", logoutUser);
 
 export default router;
